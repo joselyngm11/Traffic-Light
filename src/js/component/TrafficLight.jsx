642fd090af71308e9af0bd8e0e975704a533981a
joselyngm11 <joselyngm.47@gmail.com>
2022-09-16 20:35:14 +0000

    Extras and style

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -2,14 +2,47 @@ import React, { useState } from "react";
 
 //create your first component
 const TrafficLight = () => {
+
+    const [ selectedColor, setSelectedColor ] = useState(undefined)
+    const [ addlight, setAddlight ] = useState(false)
+
+    const handleLight = (light)=>{
+        //setAdd(!add)
+        if(selectedColor == light){
+			setSelectedColor(undefined)
+		} else {
+			setSelectedColor(light)
+		}
+    }
+
+    const handleChange = ()=>{
+		let colors = ["red","yellow","green","purple"]
+		let i = 0
+		setInterval(()=>{
+			if (addlight){
+				setSelectedColor(colors[i%4])
+			} else {
+				setSelectedColor(colors[i%3])
+			}
+			i++
+		},1000)}
+
 	return (
-		<div className="container text-center">
+		<div className="container-fluid text-center p-0">
+            <h1 className="text-white titulo">Traffic Light</h1>
             <div className="base"></div>
-            <div className="traffic-light">
-                <div className="light">a</div>
-                <div className="light">b</div>
-                <div className="light">c</div>
+            <div className={addlight ? "traffic-light traffic-light-with-purple" : "traffic-light"}>
+                <div className={"light red mt-1 " + (selectedColor === "red" ? " glow" : "opaco")} 
+                onClick={() => handleLight("red")}></div>
+                <div className={"light yellow " + (selectedColor === "yellow" ? " glow" : "opaco")}
+                onClick={() => handleLight("yellow")}></div>
+                <div className={"light green " + (selectedColor === "green" ? " glow" : "opaco")}
+                onClick={() => handleLight("green")}></div>
+                {addlight && (<div className={"light purple " + (selectedColor === "purple" ? " glow" : "opaco")} 
+                onClick={() => handleLight("purple")}></div>)}
 		    </div>
+            <button type="button" className="boton btn btn-dark" onClick={()=>handleChange()}>Change</button>
+            <button type="button" className="boton btn btn-dark" onClick={()=>setAddlight(!addlight)}>Add purple light</button>
         </div>
 	);
 };
